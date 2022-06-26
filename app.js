be49db57a2f5560cb175766724e582ffb49530b5
Nick L <nick-laopeifa@outlook.com>
2022-06-26 16:58:03 -0700

    Add Newletter

---
+++ app.js
@@ -0,0 +1,66 @@
+const express= require("express");
+const bodyParser = require("body-Parser");
+const request = require("request");
+const https = require("https");
+const app =express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/signup.html");
+});
+
+
+//server pick up the information here
+app.post("/",function(req,res){
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status:"subscribed",
+        merge_fields:{
+          FNAME:firstName,
+          LNAME:lastName
+        }
+      }
+    ]
+  };
+  const jsonData = JSON.stringify(data);
+  const url = "https://us11.api.mailchimp.com/3.0/lists/399b4ae5a5";
+
+  const options = {
+    method:"POST",
+    auth:"Peifa:d89c0233b9715d82ae793428ef5bbcee-us11"
+  }
+  const request = https.request(url,options,function(response){
+
+    if(response.statusCode == 200){
+      res.sendFile(__dirname+"/success.html");
+    }
+    else{
+      res.sendFile(__dirname+"/faulure.html");
+    }
+
+    response.on("data",function(data){
+      console.log(JSON.parse(data));
+    })
+  })
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure",function(req,res){
+  res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000,function(){
+  console.log("Server is listening on port 3000");
+});
+
+
+//API key d89c0233b9715d82ae793428ef5bbcee-us11
+//ListId
+//399b4ae5a5
